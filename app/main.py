d282978c2ac9a483ad79b6a27c970d8ae2cbbf24
cristianL0pez <cristianlopezvalpo@gmail.com>
2023-05-27 22:19:33 -0400

    v0.0.3

--- app/main.py
+++ app/main.py
@@ -154,8 +154,8 @@ async def analisis_paciente(request: Request):
 
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+""" if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000) """
 
 
 
